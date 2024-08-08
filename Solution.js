01ee3de96c9422e9db4beb0e1deda008322c7dae
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-08-08 20:29:58 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number[]} hours
+ * @return {number}
+ */
+var countCompleteDayPairs = function (hours) {
+    const _24_HOURS = 24;
+    const remainderHours = new Array(_24_HOURS).fill(0);
+    let countCompleteDayPairs = 0;
+
+    for (let current of hours) {
+        let remainder = current % _24_HOURS;
+        let complementaryTo24Hours = (remainder !== 0) ? (_24_HOURS - (current % _24_HOURS)) : 0;
+        countCompleteDayPairs += remainderHours[complementaryTo24Hours];
+        ++remainderHours[remainder];
+    }
+    return countCompleteDayPairs;
+};
